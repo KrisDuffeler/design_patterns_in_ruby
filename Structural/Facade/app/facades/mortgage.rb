246aba353a4abf359544caa67f4feb70bdfc8765
Kris Duffeler <kris.duffeler@gmail.com>
2016-01-15 09:08:40 +0100

    Facade pattern

---
+++ Structural/Facade/app/facades/mortgage.rb
@@ -0,0 +1,17 @@
+class Mortgage
+  def initialize
+    @bank = Bank.new
+    @loan = Loan.new
+    @credit = Credit.new
+  end
+
+  def is_eligible(customer, amount)
+    puts "#{customer.name} applies for #{amount} loan\n"
+
+    return false unless @bank.has_sufficient_savings(customer, amount)
+    return false unless @loan.has_no_bad_loans(customer)
+    return false unless @credit.has_good_credit(customer)
+
+    true 
+  end
+end
